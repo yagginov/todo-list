e24243628772c325f70b9482386cfefd8eb10cfb
Bohdan Yagginov <bhders62@gmail.com>
2025-08-20 09:57:55 +0300

    setup project and added models

---
+++ todo_list/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=63)
+
+
+class Task(models.Model):
+    content = models.TextField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True)
+    is_done = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, related_name="tasks")
